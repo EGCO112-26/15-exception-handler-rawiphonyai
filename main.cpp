1f9e1f91f41e408658271011b64098f12e4cb005
rawiphonyai <rawiphon.yai@student.mahidol.edu>
2026-04-20 09:34:02 +0000

    Week 15 : Exception

--- main.cpp
+++ main.cpp
@@ -3,46 +3,69 @@ using namespace std;
 
 void check0(int);
 
+class div0:public exception{ // class จัดการตัวฐานเป็น 0
+	public:
+	virtual const char* what() const throw() {
+            return "Divide by zero";
+	}
+};
+
+class fail:public exception{
+	public:
+	virtual const char* what() const throw() {
+		cin.clear();  
+		cin.ignore(50,'\n');
+		return "Accepting numbers only";
+	}
+}f; 
+
+class range:public exception{
+	public:
+	virtual const char* what() const throw() {
+		return "Value out of range";
+	}
+}r;
+
 int main(){
-	int x,y,a;
+	int x,y;
 	double d;
+	int a=1;
 do{
-	a=1;
 	try{
-		int i;	
-		double * myarray;
-		for( i=0;i<100000;i++){
-			cout<<"Allocating my array number "<<i<<endl;
-			myarray = new double[500000000];
-		}
 		cout<<"Enter 2 numbers : ";
 		cin>>x>>y;
 		/*corated Exception Handling*/
-		if(cin.fail()) throw 101;
-		if(abs(x)>1000 ||abs(y)>1000) throw("Value out of range");
+		if(cin.fail()) throw f;
+		if(abs(x)>1000 ||abs(y)>1000) throw r;
 		check0(y);
-		//if (y==0) throw "Error divide by zero";
-		a=0;
+		
 		/* Normal Code */
 		d=(double) x/y;
 		cout<<"the result is "<<d<<endl;
+		a=0;
 
+		int i;	
+		double * myarray;
+		for( i=0;i<100000;i++){
+			cout<<"Allocating memory... "<<i<<endl;
+			myarray = new double[5000000000];
+		}
 	}
 
-	catch(const char* error){
+	/*catch(const char* error){
 		cerr<<error<<endl;
 	}
 	catch(int e){
 		switch(e){
-			case 0 : cerr<<"Error divide by zero"<<endl; break;
+			case 0   : cerr<<"Error divide by zero"<<endl; break;
 			case 101 : cerr<<"Incorrect Numbers"<<endl; break; 
 		}
 		cin.clear();  //only first
 		cin.ignore(50,'\n'); //clear all
-	}
-	catch(...){
-		cerr<<"Memory Leak!!!";
-		a=1;
+	}*/
+
+	catch(exception &e){
+		cerr<<e.what()<<endl;
 	}
 
 }while(a);
@@ -52,6 +75,7 @@ do{
 
 
 void check0(int x){
-	if(x==0) throw(x);
-
+	div0 e;
+	if(x==0) throw(e);
 }
+
